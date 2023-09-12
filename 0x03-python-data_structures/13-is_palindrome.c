fdb11ef0e2c44aeaf14184b84d6e05f279f6026f
malukata488 <malukatagallet@gmail.com>
2023-09-12 06:04:18 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,47 +1,75 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if the linked list is palindrome or not
- * @head: pointer to head of list
- * Return: 0 for not being palindrome, 1 for palindrome
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
  */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *top = *head;
-	listint_t *current;
-	listint_t *mirror;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (top == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	current = top->next;
-
-	while (current != NULL)
+	while (1)
 	{
-		if  (top == mirror || top->next == mirror)
-			return (1);
-
-		if (top->n == current->n)
+		fast = fast->next->next;
+		if (!fast)
 		{
-			if (top == *head)
-			{
-				if (current->next != NULL)
-					return (0);
-			}
-			else
-			{
-				if (current->next != mirror)
-					return (0);
-			}
-			top = top->next;
-			mirror = current;
-			current = top->next;
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
 		}
 		else
-			current = current->next;
+			return (0);
 	}
-	if (top->next == mirror || top->next == NULL || top == mirror)
+
+	if (!dup)
 		return (1);
 
 	return (0);
